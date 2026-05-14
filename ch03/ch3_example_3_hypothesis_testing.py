d1012874252287e5bf6c7cc3466ff79139aa6f32
feminyalcin <45672003+feminyalcin@users.noreply.github.com>
2026-05-14 16:31:21 +0300

    Update ch3_example_3_hypothesis_testing.py

--- ch03/ch3_example_3_hypothesis_testing.py
+++ ch03/ch3_example_3_hypothesis_testing.py
@@ -1,4 +1,4 @@
-# ch3_example_3_hypothesis_testing.py
+a# ch3_example_3_hypothesis_testing.py
 # ============================================================
 # Example 3.3
 # Hypothesis Testing Across Domains
